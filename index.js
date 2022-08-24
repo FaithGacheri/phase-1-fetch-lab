a45aadc25495fc2530f2ede1c39464d61acfd1f6
“[firstname <faith.gacheri@student.moringaschool.com>
2022-08-25 01:40:13 +0300

    Assignment Completed

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then((response) => response.json())
+  .then((data) => {
+   renderBooks(data)
+  })
 }
 
 function renderBooks(books) {
@@ -9,9 +13,11 @@ function renderBooks(books) {
     const h2 = document.createElement('h2');
     h2.innerHTML = book.name;
     main.appendChild(h2);
+   
   });
 }
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
+  
 });
